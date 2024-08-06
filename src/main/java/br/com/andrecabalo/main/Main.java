a2886dcf8fe4b81d4afba563d4bc657032af8be2
Andre Cabalo <andreccabalo@gmail.com>
2024-08-05 21:31:22 -0300

    Testando comando git pela terminal

--- src/main/java/br/com/andrecabalo/main/Main.java
+++ src/main/java/br/com/andrecabalo/main/Main.java
@@ -23,7 +23,7 @@ public class Main {
         }catch (RuntimeException e){
             System.out.println("Erro ao buscar o endereço: " + e.getMessage());
         }finally {
-            System.out.println("\nFim da execução do programa");
+            System.out.println("\nFim da execução deste programa");
         }
 
 
